9e924a8ea62f26c6ec69181e06c086ad9034f507
Meowharu <92733785+Meowharu@users.noreply.github.com>
2022-03-15 22:52:16 +0100

    Add files via upload

---
+++ pdLab2/main.py
@@ -0,0 +1,94 @@
+# zad 1
+
+# lista = ['pilka nozna', 'siatkowka', 'football']
+# lista.reverse()
+# print(lista)
+# lista.append('koszykowka')
+# lista.append('tenis')
+# print(lista)
+
+# zad 2
+
+# slownik = {'itd':'i tak dalej', 'itp':'i temu podobne', 'ww':'wyzej wymieniony', 'tj':'to jest'}
+# print(slownik.keys())
+# print(slownik.values())
+
+# zad 3
+
+# gry = {'League of legends':'Riot games', 'Minecraft':'Microsoft', 'Elden ring':'From Software'}
+# a = 0
+# b = 0
+# for a in gry:
+#     b+=1
+# print(b)
+
+# zad 4
+
+# ile = 0
+# napis = input('Wpisz dowolne zdanie: ')
+# print(napis)
+# for x in napis:
+#     if x == 'a':
+#         ile+=1
+# print(ile)
+
+# zad 5
+
+# a = input("podaj pierwsza liczbe")
+# b = input("podaj druga liczbe")
+# c = input("podaj trzecia liczbe")
+# a = int(a)
+# b = int(b)
+# c = int(c)
+# print(a*b+c)
+
+# zad 6
+
+# a = input("podaj pierwsza liczbe")
+# b = input("podaj druga liczbe")
+# c = input("podaj trzecia liczbe")
+# a = int(a)
+# b = int(b)
+# c = int(c)
+# if a>b:
+#     if a>c:
+#         print("Liczba a jest najewieksza")
+#     else:
+#         print("Liczba c jest najwieksza")
+# else:
+#     if b>c:
+#         print("Liczba b jest najwieksza")
+#     else:
+#         print("Liczba c jest najwieksza")
+
+# zad 7
+
+# pod = 0
+# lista = [4, 6, 7.8, 4.5, 3, 1.9, 2]
+# for x in lista:
+#     x=x
+#     print(x)
+
+# zad 8
+
+# lista = []
+# ile = 0
+# while ile != 10:
+#     ile += 1
+#     a = input('Podaj liczbe')
+#     a =int(a)
+#     if a%2 == 0:
+#         lista.append(a)
+#     else:
+#         continue
+# print(lista)
+
+# zad 9
+
+# a = input("Podaj liczbe: ")
+# a = int(a)
+# if a<0:
+#     print('zla liczba')
+# else:
+#     a = math.sqrt(a)
+#     print(a)
